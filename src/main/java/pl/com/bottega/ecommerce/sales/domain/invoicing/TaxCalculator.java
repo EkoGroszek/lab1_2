e114dea3985ddcea55d5c2d710e6ba7e4e27907d
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-20 19:03:46 +0100

    TaxCalculator class added

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -0,0 +1,8 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+public class TaxCalculator implements ITaxCalculator {
+
+    @Override public Tax calculate() {
+        return null;
+    }
+}
