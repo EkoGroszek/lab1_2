6411a6a55a4e63d6e35009ebf46aa9bb6f7b6e28
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-27 18:12:41 +0100

    testMultiplyByBigDecimal added

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/BunchOfTests.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/BunchOfTests.java
@@ -3,6 +3,8 @@ package pl.com.bottega.ecommerce.sharedkernel;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.hamcrest.CoreMatchers.is;
 
 public class BunchOfTests {
@@ -24,13 +26,21 @@ public class BunchOfTests {
 
     @Test public void testMultiplyBy() {
         double value = 30;
-        int multiplier = 3;
+        double multiplier = 3;
         Money testValue = new Money(value * multiplier);
 
         Money money = new Money(30);
 
-        System.out.println(money.multiplyBy(3));
         Assert.assertThat(money.multiplyBy(multiplier), is(testValue));
+    }
 
+    @Test public void testMultiplyByBigDecimal() {
+        BigDecimal value = new BigDecimal(30);
+        BigDecimal multiplier = new BigDecimal(3);
+        Money testValue = new Money(value.multiply(multiplier));
+
+        Money money = new Money(30);
+
+        Assert.assertThat(money.multiplyBy(multiplier), is(testValue));
     }
 }
